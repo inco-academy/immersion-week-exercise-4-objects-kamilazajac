17ad4101e8246c83716a17e8ccc2cb0583339eb2
kamilazajac <kamilaagata.zajac@gmail.com>
2022-02-10 11:12:50 +0100

    exercise 1 solved and json updated

--- src/1_has_value_checker.js
+++ src/1_has_value_checker.js
@@ -1,3 +1,15 @@
-module.exports = function hasValue() {
-  // TODO Implement me.
-};
+module.exports = hasValue;
+
+function hasValue(object, value) {
+  for (const el of Object.values(object)) {
+    if (el == value) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+console.log(hasValue({ hello: 'bonjour', my: 'mon' }, 'bonjour')); //true
+console.log(hasValue({ hello: 'bonjour', my: 'mon' }, 'hello')); //false
+console.log(hasValue({ hello: 'bonjour', my: 'mon' }, 'tasty')); //false
